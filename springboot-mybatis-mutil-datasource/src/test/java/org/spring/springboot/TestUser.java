4225d8ede81729bdc123e3bb90a400645a8ddf5b
gustary <977891679@qq.com>
2019-03-11 16:07:05 +0800

    test master branch conflict

--- springboot-mybatis-mutil-datasource/src/test/java/org/spring/springboot/TestUser.java
+++ springboot-mybatis-mutil-datasource/src/test/java/org/spring/springboot/TestUser.java
@@ -30,7 +30,7 @@ public class TestUser {
 	@Test
 	public void queryMerConfigTest() {
 
-		User user = userDao.findByName("李四");
+		User user = userDao.findByName("分支合并冲突");
 
 		System.out.println(user.getDescription());
 
