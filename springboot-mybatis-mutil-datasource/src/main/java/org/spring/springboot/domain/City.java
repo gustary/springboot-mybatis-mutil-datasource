1ce4ba89e6acd8e333ce2303b124237e3d26cc13
gustary <977891679@qq.com>
2019-03-11 10:39:52 +0800

    spring boot实现mybatis多数据源配置 初始化

---
+++ springboot-mybatis-mutil-datasource/src/main/java/org/spring/springboot/domain/City.java
@@ -0,0 +1,61 @@
+package org.spring.springboot.domain;
+
+/**
+ * 城市实体类
+ *
+ * Created by bysocket on 07/02/2017.
+ */
+public class City {
+
+	/**
+	 * 城市编号
+	 */
+	private String id;
+
+	/**
+	 * 省份编号
+	 */
+	private String provinceId;
+
+	/**
+	 * 城市名称
+	 */
+	private String cityName;
+
+	/**
+	 * 描述
+	 */
+	private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getProvinceId() {
+		return provinceId;
+	}
+
+	public void setProvinceId(String provinceId) {
+		this.provinceId = provinceId;
+	}
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
---
+++ springboot-mybatis-mutil-datasource/src/test/java/org/spring/springboot/TestUser.java
@@ -0,0 +1,43 @@
+package org.spring.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.spring.springboot.dao.cluster.CityDao;
+import org.spring.springboot.dao.master.UserDao;
+import org.spring.springboot.domain.City;
+import org.spring.springboot.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 文件名：TestUser.java
+ * 描述：
+ * 作者：KJ00019
+ * 日期：2018年11月22日下午3:36:46
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class TestUser {
+
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private CityDao cityDao;
+
+	@Test
+	public void queryMerConfigTest() {
+
+		User user = userDao.findByName("张三");
+
+		System.out.println(user.getDescription());
+
+		City city = cityDao.findByName("深圳");
+
+		System.out.println(city.getDescription());
+
+	}
+
+}
